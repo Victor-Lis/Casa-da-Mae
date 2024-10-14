ab216617cfc6ed59903202363cafa7e4ccca54ea
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-10-14 19:02:56 -0300

    feat: Iniciando tela investidores

---
+++ src/app/investidores/components/Hero/index.tsx
@@ -0,0 +1,14 @@
+import LogoImage from '@/assets/investidores/apoiadores.png'
+
+import Image from 'next/image'
+
+export default function Hero() {
+  return (
+    <div className="w-full bg-secondary px-1 md:px-20 py-16 flex flex-wrap justify-around gap-x-10">
+      <div className="flex flex-col justify-center items-center xl:flex-1">
+        <Image src={LogoImage} alt="Logo Casa da Mãe" className="w-64" />
+        <h1 className="text-center text-gray-500 text-4xl">Apoiadores</h1>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/investidores/page.tsx
@@ -0,0 +1,9 @@
+import Hero from './components/Hero'
+
+export default function Investidores() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  )
+}
