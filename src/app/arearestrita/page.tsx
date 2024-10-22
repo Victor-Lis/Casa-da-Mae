b9f71bcc6c1da590a9dfe6fea5702963db671523
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-10-22 00:16:59 -0300

    feat: Iniciando Área Restrita

---
+++ src/app/arearestrita/page.tsx
@@ -0,0 +1,7 @@
+export default function arearestrita() {
+  return (
+    <div className="w-full h-96 flex justify-center items-center bg-secondary">
+      <h2 className="text-9xl text-white uppercase">Em construção</h2>
+    </div>
+  )
+}
