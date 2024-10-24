44ae41d7134d6a3f15329582f44b8468bc322d47
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-10-23 23:40:24 -0300

    fix: forma que as props são passadas para o componente Courses

--- src/app/arearestrita/components/WithSignUp/index.tsx
+++ src/app/arearestrita/components/WithSignUp/index.tsx
@@ -7,7 +7,7 @@ export default function WithSignUp({ user }: { user: UserProps }) {
   return (
     <div className="w-full flex flex-col justify-center items-center flex-wrap py-10 gap-y-10">
       <Profile user={user} />
-      <Courses user={user} />
+      <Courses email={user.email as string} />
     </div>
   )
 }
